f27eab81da0b6e8ea7a18385a37ab47b11b5b6f7
Administrator <237303802@qq.com>
2022-10-10 13:18:52 +0800

    修复文件上传数据处理的bug

--- upload_file_fun/data_processing.py
+++ upload_file_fun/data_processing.py
@@ -1,5 +1,4 @@
 import sys
-
 import yaml
 
 
@@ -8,17 +7,13 @@ def update_spof_scenario(file_path):
         t1_data = yaml.load(f1, Loader=yaml.FullLoader)
         storageclass_name = t1_data.get('storageclass_name')
         pvc_size = t1_data.get('pvc_size')
-        yaml_config1 = {'apiVersion': 'v1',
-                        'kind': 'PersistentVolumeClaim',
-                        'metadata': {'name': 'spofblkpvc'},
-                        'spec': {'accessModes': ['ReadWriteMany'],
-                                 'resources': {'requests': {'storage': pvc_size}},
-                                 'storageClassName': storageclass_name,
-                                 'volumeMode': 'Block'
-                                 }
-                        }
-    with open(sys.path[0] + '/kraken/kraken/kubernetes/res_file/scenarios_pvc.yaml', 'w', encoding='utf-8') as sb:
-        yaml.dump(yaml_config1, sb)
+    with  open(sys.path[0] + '/kraken/kraken/kubernetes/res_file/scenarios_pvc.yaml', 'r', encoding='utf-8') as sb1:
+        doc = yaml.full_load(sb1)
+        doc['ReadWriteManyresources']['requests']['storage'] = pvc_size
+        doc['spec']['storageClassName'] = storageclass_name
+
+    with open(sys.path[0] + '/kraken/kraken/kubernetes/res_file/scenarios_pvc.yaml', 'w', encoding='utf-8') as sb2:
+        yaml.dump(doc, sb2)
         t1_data.pop('pvc_size')
         t1_data.pop('storageclass_name')
         with open(sys.path[0] + '/kraken/scenarios/spof_scenario.yaml', 'w', encoding='utf-8') as ss:
@@ -29,17 +24,14 @@ def update_spof_pvc_scenario(file_path):
         t2_data = yaml.load(f2, Loader=yaml.FullLoader)
         storageclass_name = t2_data.get('storageclass_name')
         pvc_size = t2_data.get('pvc_size')
-        yaml_config2 = {'apiVersion': 'v1',
-                        'kind': 'PersistentVolumeClaim',
-                        'metadata': {'name': 'spofblkpvc'},
-                        'spec': {'accessModes': ['ReadWriteMany'],
-                                 'resources': {'requests': {'storage': pvc_size}},
-                                 'storageClassName': storageclass_name,
-                                 '#volumeMode': 'Block'
-                                 }
-                        }
+
+    with  open(sys.path[0] + '/kraken/kraken/kubernetes/res_file/spof_blkpvc.yaml', 'r', encoding='utf-8') as sb:
+        doc = yaml.full_load(sb)
+        doc['spec']['resources']['requests']['storage'] = pvc_size
+        doc['spec']['storageClassName'] = storageclass_name
+
     with open(sys.path[0] + '/kraken/kraken/kubernetes/res_file/spof_blkpvc.yaml', 'w', encoding='utf-8') as sp:
-        yaml.dump(yaml_config2, sp)
+        yaml.dump(doc, sp)
         t2_data.pop('pvc_size')
         t2_data.pop('storageclass_name')
         with open(sys.path[0] + '/kraken/scenarios/spof_pvc_scenario.yaml', 'w', encoding='utf-8') as sps:
